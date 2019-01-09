5377c74bfdcc1f24877df1123126e86617e1524e
seb yeates <yeatessa@gmail.com>
2019-01-09 16:32:21 +0000

    change of Javascript and stop to HAl

--- js/app.js
+++ js/app.js
@@ -4,12 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
   let player = 'X'
   const refresh = document.querySelector('button')
   const gameBoard = document.querySelector('.game')
-  const boards = gameBoard.querySelectorAll('.small-board')
+  const boards = Array.from(gameBoard.querySelectorAll('.small-board'))
   const results = document.querySelector('.results')
-  const hal = document.querySelector('.hal')
-
-  let hasHal = false;
-
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [2,5,8],
+    [0,3,6],
+    [1,4,7],
+    [6,4,2],
+    [0,4,8]
+  ]
+  const mainGrid = [
+    '','','',
+    '','','',
+    '','',''
+  ]
+  // const hal = document.querySelector('.hal')
 
   //--- If click in a div that dose not have class 'current' inner html does not change ---
 
@@ -23,13 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function handleClick(e) {
     const boardThatWasClickedOn = e.target.parentNode
-    if(!boardThatWasClickedOn.classList.contains('firstclick'))
-      if(!boardThatWasClickedOn.classList.contains('current')) return false
-    if(boardThatWasClickedOn.classList.contains('taken')) return false
+    if(!boardThatWasClickedOn.classList.contains('current')) return false
     if(e.target.classList.contains('taken')) return false
     //--- Array of tiles is built from the parentNode ---
-    const tiles = Array.from(e.target.parentNode.children)
+    const tiles = Array.from(boardThatWasClickedOn.children)
     const index = tiles.indexOf(e.target)
+    const boardIndex = boards.indexOf(boardThatWasClickedOn)
 
 
     //--- Using the event target this Changes in the HTML of Child to X|| O---
@@ -37,92 +48,58 @@ document.addEventListener('DOMContentLoaded', () => {
     //--- Adds the class Taken so the cell cant be changed ---
     e.target.classList.add('taken', 'animated', 'flip')
 
-    // Check for Win
-    checkWin(e)
+    // //---Check win ---
+
+    const grid = tiles.map(tile => tile.innerHTML)
+    if(checkWin(grid)) {
+      mainGrid[boardIndex] = player
+      boardThatWasClickedOn.classList.add('taken')
+      console.log(checkWin(mainGrid))
+    }
 
     // --- Switch between players ---
     player = player === 'O' ? 'X' : 'O'
     // results turn
-    player === 'O' ? results.innerHTML = 'O Turn' : results.innerHTML = 'X Turn'
+    results.innerHTML = (player === 'O') ? 'O Turn' : 'X Turn'
     // --- This removes the old 'current' class and appies it to the new bord classas 'current'---
     // This removes the current class from the previous clicked on square
     //applies it to the new clicked square unless it has the class of taken.
 
-    const hasClass = e => e.classList.contains('taken')
-    const childArray = Array.from(boards[index].children)
+    // const hasClass = e => e.classList.contains('taken')
+    // const childArray = Array.from(boards[index].children)
+    if(tiles.every(tile => tile.classList.contains('taken'))) {
+      boardThatWasClickedOn.classList.add('taken')
+    }
 
+    boards.forEach(board => board.classList.remove('current'))
     boardThatWasClickedOn.classList.remove('current')
 
     if (boards[index].classList.contains('taken')){
-      boardThatWasClickedOn.classList.add('current')
-    }
-
-    if (boards[0].classList.contains('firstclick')){
-      console.log('contains first click')
-      boards.forEach(board => board.classList.remove('firstclick'))
-    }
-
-    // if (board => board.classList.contains('owin' 'xwin'))
-
-    if (childArray.every(hasClass)) {
-      return boardThatWasClickedOn.classList.add('current')
+      boards.forEach(board => {
+        if(!board.classList.contains('taken')) board.classList.add('current')
+      })
     } else {
-      return boards[index].classList.add('current')
+      boards[index].classList.add('current')
     }
 
 
   }
 
 
-  // //---Check win ---
-  const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [2,5,8],
-    [0,3,6],
-    [1,4,7],
-    [6,4,2],
-    [0,4,8]
-  ]
+  function checkWin(grid){
 
-  function checkWin(e){
-
-    const cells = Array.from(e.target.parentNode.children)
+    const linesToCheck = lines.map(line => line.map(index => grid[index]))
     // checks each line
     // if some of them, (ie 1 or more lines) are winning lines returns true
     // otherwise returns false
-
-    return lines.some(line => {
-
+    return linesToCheck.some(line => {
       // gets the moves of that line eg: ['X', '', 'X'], ['O', 'X', '']
-      const rowOfMoves = line.map(index => cells[index].innerHTML)
-
       // decides if all the moves match the player eg: ['X', 'X', 'X'] === true
-      const isAWinningLine = rowOfMoves.every(move => move === player)
-      console.log(isAWinningLine)
-      if (isAWinningLine === true )
-        e.target.parentNode.classList.add('taken', 'animated', 'flip')
-      if (player === 'X' && isAWinningLine === true){
-        e.target.parentNode.classList.add('xwin')
-      }
-      if (player === 'O' && isAWinningLine === true){
-        e.target.parentNode.classList.add('owin')
-      }
+      const isAWinningLine = line.every(move => move === player)
       return isAWinningLine
-
     })
-
   }
 
-  //--- AI player --- If radio buttion at start of game is set to 'true'. hal plays as player O.
-  // const childArray = Array.from(boards[index].children)
-  //
-  // if (player === 'O' && hal.value = 'true' )
-  // function pickRandomTileInCurrent() {
-  //    Math.floor(Math.random( * childArray.length))
-  // }
-  //
 
   //---Restart Game---
 
